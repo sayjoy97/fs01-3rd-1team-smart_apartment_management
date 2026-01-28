9b40872aea96a93c2a2a284aefc01bfc82fc6431
DaOn1072 <rainbow16457@naver.com>
2026-01-28 21:07:05 +0900

    feat: 로그인한 입주민 기준으로 민원 내역 조회

--- backend/src/main/java/com/jjld/domain/complaint/controller/ComplaintController.java
+++ backend/src/main/java/com/jjld/domain/complaint/controller/ComplaintController.java
@@ -2,6 +2,7 @@ package com.jjld.domain.complaint.controller;
 
 import com.jjld.domain.complaint.dto.ComplaintAdminDetailResponse;
 import com.jjld.domain.complaint.dto.ComplaintAdminResponse;
+import com.jjld.domain.complaint.dto.ComplaintUserResponse;
 import com.jjld.domain.complaint.entity.Complaint;
 import com.jjld.domain.complaint.service.ComplaintService;
 import com.jjld.domain.complaint.service.ComplaintServiceImpl;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/complaint/api")
@@ -39,6 +42,16 @@ public class ComplaintController {
 
         return ResponseEntity.ok(
                 ApiResponse.success(complaint)
+        );
+    }
+
+    // 입주민 자신이 등록한 민원 목록 조회
+    @GetMapping("/user/complaint/list")
+    public ResponseEntity<?> getUserComplaint(@RequestParam Long houseId){
+        List<ComplaintUserResponse> userComplaint = complaintService.findByHouse_HouseId(houseId);
+
+        return ResponseEntity.ok(
+                ApiResponse.success(userComplaint)
         );
     }
 }
