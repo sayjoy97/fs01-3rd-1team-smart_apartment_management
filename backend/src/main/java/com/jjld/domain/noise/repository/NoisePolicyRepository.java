78f8004f54e43a95f4fa6e2fbfc650dbd04bca66
sumin <sumin5746@gmail.com>
2026-01-29 14:36:32 +0900

    층간소음 entity 추가

---
+++ backend/src/main/java/com/jjld/domain/noise/repository/NoisePolicyRepository.java
@@ -0,0 +1,12 @@
+package com.jjld.domain.noise.repository;
+
+import com.jjld.domain.noise.entity.NoisePolicy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface NoisePolicyRepository extends JpaRepository<NoisePolicy, Long> {
+    // 현재 활성중인 정책이 뭔지 조회
+    // isActive = true 는 항상 1개만 존재!!
+    Optional<NoisePolicy> findByIsActiveTrue();
+}
