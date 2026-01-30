8feb87e21f5df57262c9f3e9013e31b4af0f2ae3
sumin <sumin5746@gmail.com>
2026-01-30 16:45:59 +0900

    깃허브 디스코드 연결

--- backend/src/main/java/com/jjld/domain/noise/repository/NoisePolicyRepository.java
+++ backend/src/main/java/com/jjld/domain/noise/repository/NoisePolicyRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 public interface NoisePolicyRepository extends JpaRepository<NoisePolicy, Long> {
     // 현재 활성중인 정책이 뭔지 조회
     // isActive = true 는 항상 1개만 존재!!
+    //
     Optional<NoisePolicy> findByIsActiveTrue();
 }
