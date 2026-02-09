5b18afdf494690b5dc1eb4b53b10e636e1d5fcb5
DaOn1072 <rainbow16457@naver.com>
2026-02-09 10:36:55 +0900

    기존 front 파일 삭제

--- frontend/src/layouts/AuthLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function AuthLayout({ children }) {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-background">
-      {children ?? <Outlet />}
-    </div>
-  );
-}
